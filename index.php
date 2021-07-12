907e03c59defd65cde3146fc2d7000ea9f3d6f41
andrewlaidlaw <36501534+andrewlaidlaw@users.noreply.github.com>
2021-07-12 17:56:30 +0100

    Testing Guzzle

--- index.php
+++ index.php
@@ -4,5 +4,13 @@ echo "<h1>Server Info</h1>
 <p>Search for a server model by machine type / model:</p>
 ";
 
+$client = new GuzzleHttp\Client();
+$res = $client->get('http://nodejs-mongodb-reader/findall?commonName=E980');
+echo $res->getStatusCode();           // 200
+echo $res->getHeader('content-type'); // 'application/json; charset=utf8'
+echo $res->getBody();                 // {"type":"User"...'
+var_export($res->json());             // Outputs the JSON decoded data
+
+
 require_once('page-footer.php');
 ?>
