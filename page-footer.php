3d01bf99c51821d6947e6104e8de36da1bf1526d
andrewlaidlaw <36501534+andrewlaidlaw@users.noreply.github.com>
2021-07-12 17:25:36 +0100

    Initial Commit

---
+++ page-footer.php
@@ -0,0 +1,7 @@
+</body>
+<footer>
+    <ul>
+        <li><a href="https://github.com/andrewlaidlaw/php-server-info">Github Page</a></li>
+</ul>
+</footer>
+</html>
---
+++ page-header.php
@@ -0,0 +1,5 @@
+<html>
+    <head>
+        <title>Power Server Info</title>
+</head>
+<body>
