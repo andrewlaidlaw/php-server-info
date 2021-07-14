96765182ffebaeb54a7a2e94fe24be2166579cb7
andrewlaidlaw <36501534+andrewlaidlaw@users.noreply.github.com>
2021-07-14 18:14:37 +0100

    More styling

--- functions.php
+++ functions.php
@@ -13,25 +13,27 @@ function drawLine($server) {
 }
 
 function drawTable($servers) {
-    echo '<table>
+    echo '<div class="ds-table-container">
+    <table class="ds-table-compact ds-striped ds-hover">
     ';
     echo '<tr><th>Cores</th><th>CPW</th><th>rPerf ST</th><th>rPerf SMT2</th><th>rPerf SMT4</th><th>rPerf SMT8</th></tr>
     ';
     foreach($servers as $server) {
         drawLine($server);
     }
-    echo '</table>
+    echo '</table></div>
     ';
 }
 
 function renderDates($dates) {
-    echo '<table>
+    echo '<div class="ds-table-container">
+    <table class="ds-table">
     ';
     echo '<tr><th>Announcement</th><th>General Availability</th><th>Withdrawn from Marketing</th><th>End of Support</th></tr>
     ';
     echo '<tr><td>' . $dates->announce . '</td><td>' . $dates->available . '</td><td>' . $dates->wdfm . '</td><td>' . $dates->eos . '</td></tr>
     ';
-    echo '</table>';
+    echo '</table></div>';
 }
 
 ?>
--- index.php
+++ index.php
@@ -4,24 +4,27 @@ require_once('page-header.php');
 require('vendor/autoload.php');
 
 // Opening page
-echo '<h1>Server Info</h1>
-<p>Search for a server model by machine type / model:</p>
+echo '<h2 class="ds-heading-2 ds-col-8">Server Info</h2>
+<p class="ds-col-8">Search for a server model by machine type / model:</p>
 ';
 
 // Create form that provides data for this page to process
 echo '
+<div class="ds-row">
 <form action="index.php" method="post">
-<div class="ds-input-container ds-col-6 ds-mar-b-2">
+<div class="ds-input-container ds-col-4">
     <label for="model" class="ds-input-label">Model:</label>
     <input type="text" class="ds-input" name="model" placeholder="9117">
 </div>
-<div class="ds-input-container ds-col-6 ds-mar-b-2">
+<div class="ds-input-container ds-col-4">
     <label for="type" class="ds-input-label">Type:</label>
     <input type="text" class="ds-input" name="type" placeholder="MMD">
 </div>
-<div class="ds-input-container ds-col-12 ds-mar-b-2">
-<input type="submit" class="ds-button ds-primary ds-text-align-center" label="Look Up">
+<div class="ds-input-container ds-col-8">
+    <input type="submit" class="ds-button ds-primary ds-text-align-center" label="Look Up">
+</div>
 </form>
+</div>
 ';
 
 $model = $_POST['model'];
@@ -31,12 +34,12 @@ $modelType = $model . '-' . $type;
 if($model && $type) {
 
     // Simple response
-    echo '<p class="ds-heading-5">Machine Type / Model is: ' . $modelType . '</p>
-    <div class="ds-hr ds-mar-b-2_5"></div>
+    echo '<h2 class="ds-heading-2">Machine Type / Model is: ' . $modelType . '</h2>
+    <div class="ds-hr ds-mar-b-2"></div>
     ';
 
     // Section to create table of rPerf and CPW figures
-    echo '<h3>Performance Figures</h3>
+    echo '<h3 class="ds-heading-3">Performance Figures</h3>
     <p>';
     $rperfclient = new GuzzleHttp\Client([ 'base_uri'=>'http://nodejs-mongodb-reader:8080/']);
     $rperfresponse = $rperfclient->request('GET', 'findall?modelType=' . $modelType);
@@ -50,7 +53,7 @@ if($model && $type) {
     $smclient = new GuzzleHttp\Client([ 'base_uri'=>'http://smfinder:8080/']);
     $smresponse = $smclient->request('GET', '?mtm=' . $modelType);
     $smurl = $smresponse->getBody();
-    echo '<h3>Sales Manual Link</h3>
+    echo '<h3 class="ds-heading-3">Sales Manual Link</h3>
     <p><a href="' . $smurl . '">' . $servers[0]->commonName . ' (' . $modelType .') sales manual</a></p>
     ';
 
@@ -59,6 +62,7 @@ if($model && $type) {
     $srresponse = $srclient->request('GET', '?url=' . $smurl);
     $srdetails = $srresponse->getBody();
     $dates = json_decode($srdetails,false);
+    echo '<h3 class="ds-heading-3">Important Dates</h3>';
     renderDates($dates);
 
 }
