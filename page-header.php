acec206fe90a52c9fdbe6ae48f903a53482c3d9f
andrewlaidlaw <36501534+andrewlaidlaw@users.noreply.github.com>
2021-07-14 18:52:14 +0100

    Adding page header

--- page-header.php
+++ page-header.php
@@ -5,11 +5,17 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="Content-Language" content="EN" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="https://www.ibm.com/favicon.ico" />
     <title>Power Server information</title>
     <link rel="stylesheet" href="https://w3.ibm.com/w3ds/cdn/latest/w3ds.css" type="text/css">
     </head>
   <body class="ds-has-sticky-footer">
     <div class="ds-grid">
+      <div class="ds-row ds-bg-blue-primary-4 ds-col-xs-12 ds-col-lg-12 ds-col-xl-12 ds-text-align-center">
+          <div class="ds-col-xs-10 ds-offset-xs-1">
+              <img src="./power-logo.png" width=33% class="ds-padding-bottom-1" alt="IBM Power">
+          </div>
+      </div>
       <div class="ds-row">
-        <div class="ds-col-12">
+        <div class="ds-col-10">
           <div class="ds-row ds-pad-t-3">
